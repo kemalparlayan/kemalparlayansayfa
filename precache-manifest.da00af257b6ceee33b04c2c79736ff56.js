39cbeea1abd85a1e6ecd30f26e2e40a8879389a0
kmlprlyn <parlayankemal@gmail.com>
2020-02-29 23:55:09 +0300

    Updates

---
+++ precache-manifest.da00af257b6ceee33b04c2c79736ff56.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16ca1d91e9593f42835c0af9d05e867a",
+    "url": "/kemalparlayansayfa/index.html"
+  },
+  {
+    "revision": "4c7ca3fc87a356804504",
+    "url": "/kemalparlayansayfa/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "60f9f2bb536055633add",
+    "url": "/kemalparlayansayfa/static/css/main.c5e6c98f.chunk.css"
+  },
+  {
+    "revision": "4c7ca3fc87a356804504",
+    "url": "/kemalparlayansayfa/static/js/2.362abcff.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/kemalparlayansayfa/static/js/2.362abcff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60f9f2bb536055633add",
+    "url": "/kemalparlayansayfa/static/js/main.197c22a1.chunk.js"
+  },
+  {
+    "revision": "060473dffa1c6990543d",
+    "url": "/kemalparlayansayfa/static/js/runtime-main.b1611b32.js"
+  },
+  {
+    "revision": "f5823f74744b1936c098c61c70125613",
+    "url": "/kemalparlayansayfa/static/media/profil-foto.f5823f74.jpg"
+  }
+]);
